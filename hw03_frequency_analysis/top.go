38cc6f452ef08e9dc209b07818259420e8d5670b
Ramil Minyukov <rrminyukov@avito.ru>
2023-12-24 14:42:13 +0300

    HW 3 Without asterisk

--- hw03_frequency_analysis/top.go
+++ hw03_frequency_analysis/top.go
@@ -1,6 +1,39 @@
 package hw03frequencyanalysis
 
-func Top10(_ string) []string {
-	// Place your code here.
-	return nil
+import (
+	"sort"
+	"strings"
+)
+
+const AmountWords = 10
+
+func Top10(inputText string) []string {
+	frequencyMap := make(map[string]int)
+
+	words := strings.Fields(inputText)
+
+	for _, word := range words {
+		frequencyMap[word]++
+	}
+
+	uniqueWords := make([]string, 0)
+
+	for k := range frequencyMap {
+		uniqueWords = append(uniqueWords, k)
+	}
+
+	sort.Slice(uniqueWords, func(i, j int) bool {
+		firstElement := uniqueWords[i]
+		firstElementAmount := frequencyMap[firstElement]
+		secondElement := uniqueWords[j]
+		secondElementAmount := frequencyMap[secondElement]
+
+		// Если количество одинаковое, то сортируем лексикографически
+		if firstElementAmount == secondElementAmount {
+			return firstElement < secondElement
+		}
+		return firstElementAmount > secondElementAmount
+	})
+
+	return uniqueWords[:AmountWords]
 }
